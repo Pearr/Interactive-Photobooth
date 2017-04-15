6d2b27a35d8f501a29b7a0792fc0365ce6dbd3fc
Joshua <joshua-247@hotmail.co.uk>
2017-04-15 21:08:28 +0100

    First Draft UI
    First 'draft' of the UI is now somewhat complete. No major breakthroughs
    but still getting used to using GitHub and Unity (hehe).
    
    Done:
    Button icons added.
    Panel implemented to group the various clothing buttons together,
    situated at the top of the window.
    
    To do:
    Unfortunatley most of the buttons still aren't functional, thinking
    about implementing a grid overlay that comes up on click for users to
    select the different type of shirt etc. Will be working on this next.
    Need icon for hand props?
    Tooltips?
    Some of the spacing, styling etc. Might need some work unless everyone
    is happy with the way it is.

---
+++ Assets/Scripts/DynamicBottomBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+[ExecuteInEditMode]
+[RequireComponent(typeof(GridLayoutGroup))]
+
+
+public class DynamicBottomBar : MonoBehaviour
+{
+
+    public int col, row;
+
+
+    void Update()
+    {
+        RectTransform Bottom_Nav_Panel = gameObject.GetComponent<RectTransform>();
+        GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup>();
+
+        grid.cellSize = new Vector2(Bottom_Nav_Panel.rect.width / col, Bottom_Nav_Panel.rect.height / row);
+    }
+}
